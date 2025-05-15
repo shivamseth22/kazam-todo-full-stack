11ae1f43117cdd148cc2b9fc4d5abe1aeb28eec0
shivamseth22 <shivamseth221104@gmail.com>
2025-05-15 23:49:40 +0530

    middleware removed

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,34 +1,34 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
+// // middleware.ts
+// import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const {  cookies } = request;
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+//   const {  cookies } = request;
 
-  console.log(pathname)
-    const sessionCookie = request.cookies.get("token")?.value;
-  console.log("Middleware check - pathname:", pathname, "session cookie:", sessionCookie);
-
-
-  if(pathname === "/" && sessionCookie){
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+//   console.log(pathname)
+//     const sessionCookie = request.cookies.get("token")?.value;
+//   console.log("Middleware check - pathname:", pathname, "session cookie:", sessionCookie);
 
 
-  // Protect all pages under /dashboard and /profile with session cookie
-  if (pathname.startsWith("/dashboard") || pathname.startsWith("/profile")) {
-    if (!cookies.get("token")?.value) {
-      // Redirect unauthorized users to homepage or login page
-      console.log("No session cookie found, redirecting to /");
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
+//   if(pathname === "/" && sessionCookie){
+//     return NextResponse.redirect(new URL("/dashboard", request.url));
+//   }
 
-  // Allow everything else
-  return NextResponse.next();
-}
 
-// Only run middleware for these routes
-export const config = {
-  matcher: ["/dashboard/:path*", "/auth/profile" , "/settings"],
-};
+//   // Protect all pages under /dashboard and /profile with session cookie
+//   if (pathname.startsWith("/dashboard") || pathname.startsWith("/profile")) {
+//     if (!cookies.get("token")?.value) {
+//       // Redirect unauthorized users to homepage or login page
+//       console.log("No session cookie found, redirecting to /");
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
+//   }
+
+//   // Allow everything else
+//   return NextResponse.next();
+// }
+
+// // Only run middleware for these routes
+// export const config = {
+//   matcher: ["/dashboard/:path*", "/auth/profile" , "/settings"],
+// };
