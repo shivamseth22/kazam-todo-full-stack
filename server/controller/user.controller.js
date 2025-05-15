370107c36b240a7cf434ac84d94ca128d0ac3c14
shivamseth22 <shivamseth221104@gmail.com>
2025-05-15 23:43:58 +0530

    cokkies bug fix

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -69,9 +69,9 @@ export const loginUser = async (req, res) => {
     // Set token in HTTP-only cookie with secure flags
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",  // only true on HTTPS in prod
-      sameSite: "strict",
-       path: "/",
+      secure: true,
+      sameSite: "none",
+      path: "/",
     });
 
     // Do NOT send the password back in response, pick only safe fields
